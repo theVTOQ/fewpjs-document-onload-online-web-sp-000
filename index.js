684d70f74296422dba17bd19834cfc41a8c164ce
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-15 03:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("text").textContent = "This is really cool!";
 });
 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
